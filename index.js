81eb159c5c07fa03bfd47ce3d644b8beadf4fe98
WissamMU <wessam.2003.ali@gmail.com>
2024-02-25 20:46:16 +0300

    1.1 and 1.2
    I just finished the interaction and the basics for java Script

---
+++ index.js
@@ -0,0 +1,12 @@
+let userName;
+userName = prompt("please enter your user name ");
+//console.log(userName);
+
+//array
+let names = ["spider" , "wessam" ,"bat"];
+//to print an array consolel.log(names);
+
+// Ternary Operator
+// condition ? value if true : value if false;
+userName != "Wessam" ? alert('wrong user name'):alert('Welcome wessam');
+
