57a89daa2cb5654e0a5b049385add5d26ab0fde6
WissamMU <wessam.2003.ali@gmail.com>
2024-02-26 00:05:52 +0300

    lesson 1.3
    loops and arrays

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-let userName;
+//lesson 1.3
+/*let count = 0;
+//while (count <5) console.log("spider" , ++count );
+ while (count <= 10)
+ {
+    if(count % 2) console.log("hello ",count);
+    count ++;
+ }
+// for (varable ; condition ; after the loop)
+for (let i = 0 ; i < 11 ; i++ )
+{
+   console.log(i)
+}
+//break live the loop
+//continue go to the start of the loop
+let totalSum = 0
+while(true)
+{
+   let sum = +prompt("sum numbers (0) to exit");
+   if (sum == 0) break;
+   if (isNaN(sum)) continue;
+   totalSum += sum;
+}
+alert(totalSum);
+
+//in for index
+//of for items 
+let boys = ["bat guy",'spider guy','silver guy']
+
+for (let index in boys) console.log(index);
+for (let members of boys) console.log(members)
+*/
+
+
+//lesson 1.1 1.2
+/*let userName;
 userName = prompt("please enter your user name ");
 //console.log(userName);
 
@@ -8,5 +43,5 @@ let names = ["spider" , "wessam" ,"bat"];
 
 // Ternary Operator
 // condition ? value if true : value if false;
-userName != "Wessam" ? alert('wrong user name'):alert('Welcome wessam');
-
+userName == "wessam" ? alert('Welcome wessam'):alert('wrong user name');
+*/
