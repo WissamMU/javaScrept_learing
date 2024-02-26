0a9d5d0cae1f38b08764e3c19e5bacfaa01a5391
WissamMU <wessam.2003.ali@gmail.com>
2024-02-26 23:49:53 +0300

    lessone 1.4
    arrays

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
+//lesson 1.4 (arrays)
+let NicNames = ['spider','bat','silver'];
+console.log(NicNames);
+//to add to the array use .unshift
+NicNames.unshift('dex');
+console.log(NicNames);
+
+//add to the end of the array
+NicNames.push('man');
+console.log(NicNames);
+
+//to add to the middel 
+//(method) Array<string>.splice(start: number, deleteCount?: number | undefined): string[] (+1 overload)
+NicNames.splice(2,0,'man')
+console.log(NicNames);
+//remove items
+NicNames.shift();//first item use shift
+console.log(NicNames);
+NicNames.pop();//last item use .pop
+console.log(NicNames);
+//form the middle .splice(startin form , how many items to remove)
+NicNames.splice(2,2);
+console.log(NicNames);
+
+//.slice to get new array .slice(from,to)
+let PartNicNames = NicNames.slice(1,4);
+console.log(PartNicNames);;
+
+//to add another array
+console.log(NicNames.concat(PartNicNames));//print both array
+console.log(NicNames); //print one array
 
 
+//search in arrays
+let NicNames1 = ['spider','bat','silver','spider'];
 
+//to look of items in the array or not 
+console.log(NicNames1.includes('spider'));//true
+console.log(NicNames1.includes('what'));//false
+
+//for getting index of a value -1 mean dosnt have the item
+console.log(NicNames1.indexOf('spider')); // 0
+console.log(NicNames1.lastIndexOf('spider')); //3
+//To get the index of the first item that fulfilled the condition
+let ones = [4,5,7,9,4,1,1,1]
+let one = ones.findIndex(ONE => ONE<2);
+console.log(one);
+//To get the first item that fulfilled the condition
+let notOne = ones.find(NOTONE => NOTONE >= 2);
+console.log(notOne);
+//To get all the items that fulfilled the condition
+let notOne2 = ones.filter(NOTONE => NOTONE >= 2);
+console.log(notOne2);
 
 
 //lesson 1.3
