8d73f4b9e77757d69c7a12de1afe688ffe7767a1
WissamMU <wessam.2003.ali@gmail.com>
2024-03-07 09:40:28 -1200

    server and things

--- node_server/server.js
+++ node_server/server.js
@@ -6,6 +6,8 @@ app.get('/',(req,res) => {
     res.sendFile('index.html' , {root: __dirname });
 });
 
+app.use('public' , express.static(__dirname + '/public'));
+
 app.get('/user',(req,res) => res.json([
     {id:1 , name: 'wessam' , email:'wessam@javascript.com'},
     {id:2 , name: 'bat' , email:'bat@javascript.com'},
@@ -15,4 +17,10 @@ app.get('/testing',(req,res) => res.json([
     {help:'me',noIDea:'what is this',orHow:'it workd'},
 ]));
 
-app.listen(3000,()=>console.log('server listen is 3000'));
+app.post('/user',(req,res) => {
+    console.log(req.body);
+    res.json({message:'user added successfully!'});
+});
+
+app.listen(3000,()=>console.log('server listen is 3000 linke in the next'+
+' line\nhttp://localhost:3000/'));
