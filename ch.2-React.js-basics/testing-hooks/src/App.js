ee8919eac575f854295bba585471f6b8fc3f652e
WissamMU <wessam.2003.ali@gmail.com>
2024-10-25 14:23:29 +0300

    start working in react finished basic and component parts

---
+++ ch.2-React.js-basics/testing-hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ClickCount from './ClickCountk';
+
+function App() {
+  return (
+    <div>
+      <ClickCount />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ch.2-React.js-basics/testing-hooks/src/ClickCountk.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';//add use effect here 
+
+const ClickCount = () => {
+    // to use use state we make array first is verable name and the second
+    // is function that goning change the verable and useState(start value to the var)
+    const [counter, setCounter] = useState(0);
+
+    // first argument is callback function that will be called 
+    // second argument is array of dependencies that useEffect will run only when one of them changes
+    useEffect(() => {
+        if (counter === 5){
+            alert('you have reached 5 times');
+        }
+    }, [counter])
+
+    return (
+        <div>
+            <p> you pressed {counter} times</p>
+            <button onClick={() => setCounter(counter + 1)}>pree me</button>
+        </div>
+    )
+}
+
+export default ClickCount;
