2a24597128e01cc5709284f18da6e11f10cfe234
WissamMU <wessam.2003.ali@gmail.com>
2024-11-01 02:34:30 +0600

    add note button ready

--- ch.2-React.js-basics/notes-site/src/App.js
+++ ch.2-React.js-basics/notes-site/src/App.js
@@ -2,31 +2,68 @@ import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Preview from './components/Preview';
+import Message from './components/Message';
 
 function App() {
+  // theme selector
   const [selectedTheme, setSelectedTheme] = useState(localStorage.getItem('selectedTheme') || 'light');
-
   const toggleTheme = () => {
     setSelectedTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
   };
-
   useEffect(() => {
     document.body.setAttribute('data-theme', selectedTheme);
     localStorage.setItem('selectedTheme', selectedTheme);
   }, [selectedTheme]);
 
 
+  // adding note state
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [creating, setCreating] = useState(false);
+  const [editing, setEditing] = useState(false);
+
+  // changing title
+  const changeTitleHandler = (event) => {
+    setTitle(event.target.value);
+  }
+
+  // changing content
+  const changeContentHandler = (event) => {
+    setContent(event.target.value);
+  }
+
+  // saving note
+  const saveNoteHandler = () => {
+    const note = {
+      id: new Date(),
+      title: title,
+      content: content,
+    }
+
+    const updatedNotes = [...notes, note];
+
+    setNotes(updatedNotes);
+    setCreating(false);
+    setSelectedNote(note.id);
+    setTitle('');
+    setContent('');
+  }
+
   const getAddNote = () => {
     return (
       <div>
-        <h2>إضافة ملاحظة جديدة</h2>
+        <h2 className='line-under mb-5'>إضافة ملاحظة جديدة</h2>
         <div>
           <input
             type="text"
             name="title"
-            className="form-input mb-30"
+            className="form-input mb-5"
             placeholder="العنوان"
-            value=""
+            value={title}
+            onChange={changeTitleHandler}
           />
 
           <textarea
@@ -34,10 +71,13 @@ function App() {
             name="content"
             className="form-input"
             placeholder="النص"
+            value={content}
+            onChange={changeContentHandler}
           />
 
-          <a href="#" className="button green">
-            حفظ
+          <a href="#" className="btn button" onClick={saveNoteHandler}>
+            حفظ 
+            <i class="fas fa-save"></i>
           </a>
         </div>
       </div>
@@ -45,45 +85,67 @@ function App() {
   };
 
   const getPreview = () => {
+    if(notes.length === 0) {
+      return <Message message='لا توجد ملاحظة'/>;
+    }
+    if(!selectedNote){
+      return <Message message='لم تحدد ملاحظة'/>;
+    }
+    const note = notes.find(note =>{
+      return note.id === selectedNote;
+    })
+
     return (
       <div>
-        <div className="note-operations">
-          <a href="#">
-            <i className="fa fa-pencil-alt" />
-          </a>
-          <a href="#">
-            <i className="fa fa-trash" />
-          </a>
+        <div className='line-under'>
+          <div className="note-operations">
+            <h2 className='note-title'>{note.title}</h2>
+            <div>
+              <a href="#">
+                <i className="fa fa-pencil-alt" />
+              </a>
+              <a href="#">
+                <i className="fa fa-trash" />
+              </a>
+            </div>
+          </div>
         </div>
         <div>
-          <h2 className='note-title'>عنوان ملاحظة تجريبية</h2>
-          <p>نص ملاحظة تجريبية</p>
+          <p>{note.content}</p>
         </div>
       </div>
     );
   };
 
+  const addNoteHandler = () => {
+    setCreating(true);
+  }
+
   return (
     <div className="container-fluid">
       <div className='row'>
         <div className="notes-section col-md-2">
-          <button className="btn add-btn">أضافة ملاحظة</button>
+          {/* adding note */}
+          <button className="btn add-btn" onClick={addNoteHandler}>أضافة ملاحظة</button>
+          {/* theme select */}
           <label className="add-btn">
-        <input
-          className='btn add-btn'
-          id='darkModeToggle'
-          onChange={toggleTheme}
-          type='checkbox'
-          defaultChecked={selectedTheme === 'dark'}
-        />
-        <span>
-          {selectedTheme === 'dark' ? (
-            <i className="fas fa-moon"></i>
-          ) : (
-            <i className="fas fa-sun"></i>
-          )}
-        </span>
-      </label>
+            <input
+              className='btn add-btn'
+              id='darkModeToggle'
+              onChange={toggleTheme}
+              type='checkbox'
+              defaultChecked={selectedTheme === 'dark'}
+            />
+            <span className='change-theme'>
+              تغيير المظهر
+              {selectedTheme === 'light' ? (
+                <i className="fas fa-moon"></i>
+              ) : (
+                <i className="fas fa-sun"></i>
+              )}
+            </span>
+          </label>
+          {/* note title */}
           <ul className="notes-list">
             <li className="note-item">ملاحظة رقم #1</li>
             <li className="note-item">ملاحظة رقم #2</li>
@@ -91,9 +153,12 @@ function App() {
             <li className="note-item">ملاحظة رقم #4</li>
           </ul>
         </div>
-        <div className="preview-section col-md-9">{getPreview()}</div>
+        {/* preview section */}
+        <Preview>
+          {creating ? getAddNote() : getPreview()}
+        </Preview>
       </div>
-    </div>
+    </div >
   );
 }
 
---
+++ ch.2-React.js-basics/notes-site/src/components/Preview/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Preview = (props) =>(
+    <div className="preview-section col-md-9">
+        {props.children}
+    </div>
+)
+
+export default Preview;
